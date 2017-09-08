184e01f38be4b91e650105ebebcba262da1f99c5
attapun-an <attapunanivat@gmail.com>
2017-09-08 09:08:52 +0700

    endOfLesson
    end of lesson. saving work for later.

--- stack.py
+++ stack.py
@@ -1,1 +1,20 @@
-def push()
+def stackInit():
+    stackList = []
+    stackLen = 5
+    for i in range(0,stackLen):
+        stackList.append("")
+    print(stackList)
+    startPointer = 0
+    endPointer = stackLen
+
+stackInit()
+
+def push(item, endPointer, stackList):
+    endPointer += 1
+    stackList[endPointer] = item
+    return item, endPointer, stackList
+
+def pop(endPointer, stackList):
+    return stackList[endPointer]
+    endPointer -= 1
+
