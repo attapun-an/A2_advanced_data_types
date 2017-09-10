834edad360a880b3cf2f638c5ef36bbf7e434ab0
attapun-an <attapunanivat@gmail.com>
2017-09-11 00:52:42 +0700

    push input interface
    Starting work on the user interface. I’ve decided it would be better
    for the push function to ask for user input since that would make more
    sense than adding it to the main loop then sending it to the function.

--- stack.py
+++ stack.py
@@ -6,18 +6,38 @@ def stackInit():
     print(stackList)
     startPointer = 0
     endPointer = stackLen
+    runStack = True
+    while runStack:
 
-#stackInit()
 
-def push(item, endPointer, stackList):
+stackInit()
+
+# adds a new item to the stack, moves the end pointer forwards.
+def push(endPointer, stackList):
+    item = input("input item: ")
     endPointer += 1
     stackList[endPointer] = item
-    return item, endPointer, stackList
+    return endPointer, stackList
 
+# we need a check to see if it has gone over the stack size
+
+# increments the endPointer back 1 space and returns it's new position and the popped value
 def pop(endPointer, stackList):
     popped = stackList[endPointer]
     endPointer -= 1
     return endPointer, popped
+
+# checks if the end pointer has gone below 0 (the first slot in the stack) - meaning there's nothing to pop
+def checkStackEmpty(endPointer, startPointer):
+    if endPointer < startPointer:
+        return True
+    else:
+        return False
+
+def displayinstructions():
+    print("""stack Instructions:
+    1) push item
+    2) """)
 
 # test on returning multiple values in a function
 """
