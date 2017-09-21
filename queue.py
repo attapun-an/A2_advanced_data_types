1446e1b7d33004b0aa5119ab9e9447cfc507974f
attapun-an <attapunanivat@gmail.com>
2017-09-21 08:44:13 +0700

    pushTest
    life is great.

--- queue.py
+++ queue.py
@@ -5,4 +5,37 @@ Now that you understand what a stack is (LIFO) do you think you could code a que
   how they will act differently as opposed to a stack.
 """
 
+# Wow, there's a lot of task to complete. I will have to prioritize speed over efficiency.
+
+""" def queue_init(queueInitLength):
+    global queueList
+    queueList = []
+    queueLen = queueInitLength
+    for i in range(0,queueLen):
+        queueList.append("")
+    print(queueList)
+    # not sure how global variables work.. Let's just go with it I guess
+    global startPointer
+    startPointer = 0
+    global endPointer
+    endPointer = -1
+    return
+"""
+
+# def push(item):
+
+# let's do a few test
+
+def pushtest():
+    testList = ["h", "e", "l", "l", "o"]
+    testList.insert(0, " ")
+    testList.insert(0, "i")
+    testList.insert(0, "h")
+    print(testList)
+pushtest()
+
+# It works! 
+
+
+
 
