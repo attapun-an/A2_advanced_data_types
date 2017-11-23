5b6d15fc21921fc480113a604468429e9b7896f6
attapun-an <attapunanivat@gmail.com>
2017-11-24 00:12:25 +0700

    OL_add_confusion
    so. by having
    current = self.head
    that would mean current:
    ==  None
    == wherever the head is pointing to

--- linked_list_ordered.py
+++ linked_list_ordered.py
@@ -13,27 +13,29 @@ class OrderedList():
     end, we get None .Loop through it until we hit None, then add it" """
     # add
     def add(self, item):
-        current = self.head
-        # keeps track of previous item
-        prev = current
-        found = False
-        # loop through till the next item is == None AND the position has not been found
-        while current.get_data(current.get_next()) is not None and found is False:
-            current = current.get_next()
-            
-            if current.get_data < item:
-                # break the loop
-                found = True
-            else:
-                prev = current
-
-        # generate node for item
-        temp = Node(item)
-        if prev == self.head:
-            prev = temp
+        if self.isEmpty():
+            temp = Node(item)
+            self.head = temp
+            temp.set_next(None)
         else:
+            current = self.head
+            # keeps track of previous item
+            prev = current
+            found = False
+            # loop through till the next item is == None AND the position has not been found
+            while current.get_data() is not None and found is False:
+                current = current.get_next()
+
+                if current.get_data() < item:
+                    # break the loop
+                    found = True
+                else:
+                    prev = current
+
+            # generate node for item
+            temp = Node(item)
             prev.set_next(temp)
-        temp.set_next(current)
+            temp.set_next(current)
 
 
 
@@ -53,6 +55,7 @@ def main():
     thisList.add(1)
     thisList.display2()
     thisList.add(2)
+    thisList
 
 
 if __name__ == '__main__':
