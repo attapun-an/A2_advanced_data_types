6b9e806c7b07f41eda414c4707cbabef2922bb57
attapun-an <attapunanivat@gmail.com>
2017-11-23 23:51:50 +0700

    ordered_add: start from scratch
    restart from scratch

--- linked_list_ordered.py
+++ linked_list_ordered.py
@@ -14,27 +14,46 @@ class OrderedList():
     # add
     def add(self, item):
         current = self.head
-        next = current
-        if self.isEmpty() is not True:
-            while next is not None:
-                next = current.get_next()
-                if next.get_data() <= item:
-                    temp = Node()
-                    temp.set_next(item)
-                    current.set_next(next)
-                current = next
-            temp = Node()
-            temp.set_next(item)
-            current.set_next(next)
-        else:
-            temp = Node(item)
-            temp.set_next(None)
-            current = self.head
+        # keeps track of previous item
+        prev = current
+        found = False
+        # loop through till the next item is == None AND the position has not been found
+        while current.get_data(current.get_next()) is not None and found is False:
+            current = current.get_next()
+            
+            if current.get_data < item:
+                # break the loop
+                found = True
+            else:
+                prev = current
 
+        # generate node for item
+        temp = Node(item)
+        if prev == self.head:
+            prev = temp
+        else:
+            prev.set_next(temp)
+        temp.set_next(current)
+
+
+
+
+
+
+    def display2(self):
+        current = self.head
+        line = ""
+        while current is not None:
+            line = line + (str(current.get_data())) + " "
+            current = current.get_next()
+        print (line)
 
 def main():
     thisList = OrderedList()
-    thisList.add(23)
+    thisList.add(1)
+    thisList.display2()
+    thisList.add(2)
+
 
 if __name__ == '__main__':
     main()
