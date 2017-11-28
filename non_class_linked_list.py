ffa6403ef125bf7822e73ba3ee0ab195fa851b1c
attapun-an <attapunanivat@gmail.com>
2017-11-28 08:51:29 +0700

    more like nested list
    2D array initialization method found

---
+++ non_class_linked_list.py
@@ -0,0 +1,35 @@
+"""
+1. Study the process and the pseudocode laid out in the attachment.
+2. Determine if you feel using arrays would be more efficient, or not,
+ than using OOP and explain why.
+3. Use the pseudocode to start programming your own linkedlists
+but without using classes (OOP) and only using arrays/lists.
+"""
+
+"""It is less efficient to use arrays, it is quite impractical to do it with arrays 
+since it gets quite messy and it fills up if we don't keep track of the things"""
+
+# VARIABLES
+NullPointer = -1
+
+# constant positions in 2D array
+data = 0
+pointer = 1
+
+
+
+# create 2d array??
+
+
+
+
+
+
+# LOGIC
+def init_list(list_name):
+    for i in range(6):
+        list_name[][]
+
+print(ordered_list)
+# Initialize list
+
---
+++ test/2dArr_test.py
@@ -0,0 +1,25 @@
+
+
+def testlist_01():
+    array = [["",""], ["",""], ["",""], ["",""], ["",""], ["",""], ["",""]]
+    array[0][0] = "improvise"
+    array[0][1] = "adapt"
+    array[1][0] = "overcome"
+    print(array)
+
+
+def testlist_02():
+    array02 = []
+    for i in range(7):
+        array02.append(["",""])
+    print(array02)
+
+"""Yay~~~ Finally found how to set up a 2D array, (it's just basically a nested list"""
+
+
+def main():
+    # testlist_01()
+    testlist_02()
+
+
+main()
